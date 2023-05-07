7685ed04283c945906ad16248ae1d330a9fb5462
sangeuns <sangeun.front@gmail.com>
2023-05-08 04:31:39 +0900

    prettier 세팅 - 저장과 동시에 prettier 적용

--- check-card-box/slide-effct-test/slide-test.js
+++ check-card-box/slide-effct-test/slide-test.js
@@ -1,16 +1,16 @@
-// slick 라이브러리
-function SliderBox1__init() {
-  $('.slider-box-1 > .slick').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: 0,
-    arrows: true,
-  });
-}
+$(document).ready(function () {
+  function SliderBox1__init() {
+    $('.slider-box-1 > .slick').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: 0,
+      arrows: true,
+    });
+  }
 
-SliderBox1__init();
+  SliderBox1__init();
+});
